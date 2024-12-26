08c70d31aa64d5a262789e1729e4771815000252
Charan Romi <charanromi325@gmail.com>
2024-12-26 14:08:04 +0530

    new steps

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ parser.add_argument('-p','--path',help='Path of Text File',required=True)
 parser.add_argument('-c','--character',help='Count Characters',action='store_true')
 parser.add_argument('-l','--line',help='Count Lines',action='store_true')
 parser.add_argument('-w','--word',help='Count Words',action='store_true')
+parser.add_argument('-m', '--mode', help='Mode of Operation', choices=['bytes', 'lines', 'words'], default='bytes')
 
 
 args = vars(parser.parse_args())
@@ -25,5 +26,15 @@ with open(args['path'], 'r', encoding='utf-8') as file:
         # Step - 03: return the number of words in the file
         elif args['word']:
             print(len(file.read().split()))
+        
+        # Step - 04: return the number of bytes, lines, or words in the file
+        elif args['mode'] == 'bytes':
+            print(os.stat(args['path']).st_size)
+        elif args['mode'] == 'lines':
+            print(len(file.readlines()))
+        elif args['mode'] == 'words':
+            print(len(file.read().split()))
+        
+        
 
 
